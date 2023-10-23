2438e2f4c6b2ea57358378df097976cbc3521e1f
Samantha Mah <stmah@ucdavis.edu>
2023-10-22 19:51:51 -0700

    madlibs

---
+++ studio1/script.js
@@ -0,0 +1,22 @@
+(function () {
+
+    'use strict';
+
+    // add script here
+    'use strict';
+    document.querySelector("#submit").addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('#overlay').className = "showing";
+    });
+
+    document.querySelector(".close").addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('#overlay').className = "hidden";    
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if(event.key === "Escape") {
+            document.getElementById('overlay').className = "hidden";
+        }
+    })
+})();
