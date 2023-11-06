cd092b7509aaf30dac00c530d3a95d3d1d90be1e
Samantha Mah <stmah@ucdavis.edu>
2023-11-06 10:41:14 -0800

    every picture

--- studio2/script.js
+++ studio2/script.js
@@ -1,3 +1,22 @@
 (function () {
     'use strict';
+
+    let pageTop;
+    const bodyTag = document.querySelector('body');
+    const hydrangeas = document.querySelector('#hydrangeas');
+
+    window.addEventListener('scroll', function() {
+
+        pageTop = window.pageYOffset;
+
+        switch(true) {
+            case pageTop < 500: bodyTag.className="one"; break;
+            case pageTop < 5000: bodyTag.className="two"; break;
+            default: bodyTag.className="one";
+
+            case pageTop < 500: hydrangeas.className="start"; break;
+            case pageTop < 5000: hydrangeas.className="end"; break;
+        }
+        
+    }) 
 }) ();
