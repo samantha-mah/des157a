278a506c43afb9db16f842f329f4011fdaf1c800
Samantha Taylor Mah <stmah@scc2103-16.ucdavis.edu>
2023-11-08 16:27:14 -0800

    every picture

--- studio2/script.js
+++ studio2/script.js
@@ -1,22 +1,36 @@
 (function () {
     'use strict';
 
+    const hydrangeas = document.querySelector('#hydrangeas');
+    const camera = document.querySelector('#camera');
     let pageTop;
     const bodyTag = document.querySelector('body');
-    const hydrangeas = document.querySelector('#hydrangeas');
 
     window.addEventListener('scroll', function() {
 
         pageTop = window.pageYOffset;
 
         switch(true) {
-            case pageTop < 500: bodyTag.className="one"; break;
-            case pageTop < 5000: bodyTag.className="two"; break;
-            default: bodyTag.className="one";
+            case pageTop < 10: hydrangeas.className="start"; camera.className="start"; break;
 
-            case pageTop < 500: hydrangeas.className="start"; break;
-            case pageTop < 5000: hydrangeas.className="end"; break;
+            case pageTop < 500: bodyTag.className="one"; break;
+             
+            case pageTop < 5000: bodyTag.className="two"; hydrangeas.className="end"; camera.className="end"; break;
+
+            default: bodyTag.className="one";
+            
         }
         
-    }) 
+    });
+
+    function zoomIn(event) {
+        event.preventDefault();
+
+        const targetID = event.target.getAttribute('#camera');
+        const targetAnchor = document.querySelector(targetID);
+
+        const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top) / 100 + 1;
+
+        
+    }
 }) ();
