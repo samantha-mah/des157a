ef8d28ea730cf76cfb97deb2ffaebb3439e09ec0
Samantha Mah <stmah@ucdavis.edu>
2023-11-20 21:36:48 -0800

    game on

--- studio3/script.js
+++ studio3/script.js
@@ -1,3 +1,24 @@
 (function () {
     'use strict';
+
+    console.log('reading js');
+
+    const yes = document.querySelector('#yes');
+    const no = document.querySelector('#no');
+    const close = document.querySelector('#close');
+
+    yes.addEventListener('click', function() {
+        document.querySelector('#overlay').className = "showing";
+    });
+
+    close.addEventListener('click', function() {
+        document.querySelector('#overlay').className = "hidden";
+    });
+
+    document.addEventListener('keydown', function(event) {
+        if(event.key === "Escape") {
+            document.getElementById('overlay').className = "hidden";
+        }
+    });
+
 }) ();
